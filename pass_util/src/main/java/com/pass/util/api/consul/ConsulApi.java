f79b3c6809ae28291d2e9b639db83290d4571f0c
yuanzhonglin <shui19971207YZL>
2019-05-24 10:04:05 +0800

    consul--API

---
+++ pass_util/src/main/java/com/pass/util/api/consul/ConsulApi.java
@@ -0,0 +1,166 @@
+package com.pass.util.api.consul;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.QueryParams;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.acl.model.AclType;
+import com.ecwid.consul.v1.acl.model.NewAcl;
+import com.ecwid.consul.v1.acl.model.UpdateAcl;
+import com.ecwid.consul.v1.agent.model.Check;
+import com.ecwid.consul.v1.agent.model.Member;
+import com.ecwid.consul.v1.agent.model.NewService;
+import com.ecwid.consul.v1.agent.model.Service;
+import com.ecwid.consul.v1.coordinate.model.Datacenter;
+import com.ecwid.consul.v1.health.model.HealthService;
+import com.ecwid.consul.v1.kv.model.GetValue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author yuanzhonglin
+ * @date 2019/5/22
+ */
+public class ConsulApi {
+
+    private static ConsulClient consulClient = initConsul();
+
+    private static ConsulClient initConsul(){
+        consulClient = new ConsulClient("192.168.32.15", 9500);
+        return consulClient;
+    }
+
+    // 增加服务
+    public static void addService(String id, String name) {
+        NewService newService = new NewService();
+        newService.setId(id);
+        newService.setName(name);
+        newService.setTags(Arrays.asList("version=1.0", "author=dfzq", "secure=false"));
+        newService.setPort(1104);
+        newService.setAddress("192.168.32.15");
+
+        NewService.Check serviceCheck = new NewService.Check();
+        serviceCheck.setHttp("http://127.0.0.1:1101/consul/health");
+        serviceCheck.setInterval("10s");
+
+        newService.setCheck(serviceCheck);
+
+        consulClient.agentServiceRegister(newService);
+    }
+
+    // 删除服务
+    public static void deleteService(String serviceId) {
+        consulClient.agentServiceDeregister(serviceId);
+    }
+
+    // 获取consul成员
+    public static void findMembers() {
+        Response<List<Member>> agentMembers = consulClient.getAgentMembers();
+        System.out.println("----findMembers----:" + JSONObject.toJSON(agentMembers.getValue()));
+    }
+
+    // 获取注册服务
+    public static void findService() {
+        Response<Map<String, Service>> services = consulClient.getAgentServices();
+        System.out.println("----findService----:" + JSONObject.toJSON(services.getValue()));
+    }
+
+    // 获取注册服务
+    public static void findCheckMsg() {
+        Response<Map<String, Check>> agentChecks = consulClient.getAgentChecks();
+        System.out.println("----findCheckMsg----:" + JSONObject.toJSON(agentChecks.getValue()));
+    }
+
+    // 根据服务名以及健康状态获取服务列表
+    public static void findHealthyService(String serviceName, boolean onlyPassing) {
+        Response<List<HealthService>> healthyServices = consulClient.getHealthServices(serviceName, onlyPassing, QueryParams.DEFAULT);
+        System.out.println("----findHealthyService----:" + JSONObject.toJSON(healthyServices.getValue()));
+    }
+
+    // 设置KV
+    public static void setKV(String key, String value) {
+        Response<Boolean> response = consulClient.setKVValue(key, value);
+        System.out.println("----setKV----:" + response.getValue());
+    }
+
+    // 查询KV
+    public static void getKV(String key) {
+        Response<GetValue> kvValue = consulClient.getKVValue(key);
+        System.out.println("----getKV----:" + kvValue.getValue().getDecodedValue());
+    }
+
+    // 删除KV
+    public static void deleteKV(String key) {
+        consulClient.deleteKVValue(key);
+    }
+
+    public static void findRaftPeers() {
+        Response<List<String>> listResponse = consulClient.getStatusPeers();
+        System.out.println("----findRaftPeers----:" + listResponse.getValue());
+    }
+
+    public static void findRaftLeader() {
+        Response<String> stringResponse = consulClient.getStatusLeader();
+        System.out.println("----findRaftLeader----:" + stringResponse.getValue());
+    }
+
+    // 获取数据中心
+    public static void getDatacenters() {
+        Response<List<String>> response = consulClient.getCatalogDatacenters();
+        System.out.println("----getDatacenters----:" + response.getValue());
+
+        Response<List<Datacenter>> datacenters = consulClient.getDatacenters();
+        System.out.println("----getDatacenters----:" +datacenters.getValue());
+    }
+
+    // createACL
+    public static void createACL() {
+        NewAcl acl = new NewAcl();
+        acl.setName("acl_1");
+        acl.setType(AclType.CLIENT);
+        acl.setRules("node\"\"{policy=\"write\"}");
+        Response<String> response = consulClient.aclCreate(acl, "6171fe30-1138-4127-9b08-60069e945550");
+
+        System.out.println("----createACL----:" + response.getValue());
+    }
+
+    // updACL
+    public static void updACL() {
+        UpdateAcl acl = new UpdateAcl();
+        acl.setId("fa5b9db4-8271-de50-28bd-321555137c10");  //token值
+        acl.setRules("node\"\"{policy=\"read\"}");
+        acl.setName("acl_demo");
+        consulClient.aclUpdate(acl, "6171fe30-1138-4127-9b08-60069e945550");
+
+        System.out.println("----updACL----");
+    }
+
+
+
+    public static void main(String[] args) {
+        // 服务
+        addService("consul-server-1104", "consul-server");
+        findService();
+        findHealthyService("consul-client", true);
+
+        // 查询
+        findMembers();
+        findCheckMsg();
+
+        // KV
+        setKV("key1", "value1");
+        getKV("key1");
+        deleteKV("key1");
+
+        findRaftPeers();
+        findRaftLeader();
+
+        // 数据中心
+        getDatacenters();
+
+        //createACL();
+        updACL();
+    }
+}
---
+++ pass_util/src/main/java/com/pass/util/restful_api/consul/ConsulApi.java
@@ -0,0 +1,100 @@
+package com.pass.util.restful_api.consul;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * consul
+ *
+ * @author yuanzhonglin
+ * @date 2019/5/22
+ */
+public class ConsulApi {
+    public static void main(String[] args) throws Exception{
+
+
+        // 获取本地注册的服务
+        getMethod("http://192.168.32.15:9500/v1/agent/services");
+
+        // 返回agent在集群中看到的成员
+        getMethod("http://192.168.32.15:9500/v1/agent/members");
+
+        // 返回本地agent注册的所有检查(包括配置文件和HTTP接口)
+        getMethod("http://192.168.32.15:9500/v1/agent/checks");
+
+        // 返回本地agent的配置和成员信息
+        getMethod("http://192.168.32.15:9500/v1/agent/self");
+
+        // 触发本地agent加入node
+        //getMethod("http://192.168.32.15:9500/v1/agent/join/192.168.32.15");
+
+        putMethod("http://192.168.32.15:9500/v1/agent/service/deregister/consul-client-1103");
+
+    }
+
+    //get
+    public static void getMethod(String url) throws Exception {
+        URL restURL = new URL(url);
+
+        HttpURLConnection conn = (HttpURLConnection) restURL.openConnection();
+
+        conn.setRequestMethod("GET"); // POST GET PUT DELETE
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        Object response = getResponse(conn);
+        System.out.println(JSONObject.toJSON(response));
+    }
+
+    //post
+    public static void postMethod(String url) throws Exception {
+        URL restURL = new URL(url);
+
+        HttpURLConnection conn = (HttpURLConnection) restURL.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        Object response = getResponse(conn);
+        System.out.println(JSONObject.toJSON(response));
+    }
+
+    //put
+    public static void putMethod(String url) throws Exception {
+//        URL restURL = new URL(url);
+//        HttpURLConnection conn = (HttpURLConnection) restURL.openConnection();
+//        conn.setRequestProperty("accept", "*/*");
+//        conn.setRequestProperty("connection", "Keep-Alive");
+//        conn.setRequestProperty("user-agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+//        conn.setDoOutput(true);
+//        conn.setDoInput(true);
+//        conn.connect();
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPut httpPut = new HttpPut(url);
+        HttpResponse response = httpClient.execute(httpPut);
+    }
+
+    public static Object getResponse(HttpURLConnection conn) throws Exception{
+        Map<String, Object> map = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line = br.readLine()) != null ){
+            sb.append(line);
+        }
+        br.close();
+
+        Object parse = JSONObject.parse(sb.toString());
+        return parse;
+    }
+}
