f2c486b88f2d0241b331cd8506e9acec0cb9e9f0
yuanzhonglin <yuanzhonglin@beyondcent.com>
2020-07-18 15:30:22 +0800

    时间工具类补充

--- pass_util/src/main/java/com/pass/util/date/DateUtils.java
+++ pass_util/src/main/java/com/pass/util/date/DateUtils.java
@@ -1,12 +1,11 @@
 package com.pass.util.date;
 
+import sun.nio.cs.CharsetMapping;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.chrono.IsoChronology;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Objects;
-import java.util.TimeZone;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.*;
@@ -559,7 +558,6 @@ public class DateUtils {
 		}
 	}
 
-
 	/**
 	 * 获取日期对应的时间戳
 	 *
@@ -571,4 +569,269 @@ public class DateUtils {
 		return Long.toString(date.getTime());
 	}
 
+	/**
+	 * 根据某一天获取这一周的天数
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static Map<String, String> getAllWeek(String date) throws Exception {
+		Map<String, String> map = new LinkedHashMap<>();
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		int[] week = {Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, Calendar.THURSDAY, Calendar.FRIDAY, Calendar.SATURDAY, Calendar.SUNDAY};
+		String[] weekStr = {"周一","周二","周三","周四","周五","周六","周日"};
+		for (int i = 0; i < week.length; i ++) {
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(sd.parse(date));
+			cal.set(Calendar.DAY_OF_WEEK, week[i]);
+			if (week[i] == Calendar.SUNDAY) {
+				cal.add(Calendar.DAY_OF_WEEK, 7);
+			}
+			map.put(weekStr[i], sd.format(cal.getTime()));
+		}
+		return map;
+	}
+
+	/**
+	 * 根据某一天获取这一月的数据
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static List<String> getAllMonth(String date) throws Exception {
+		List<String> list = new ArrayList<>();
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		//当月一号
+		Calendar startCal = Calendar.getInstance();
+		startCal.setTime(sd.parse(date));
+		startCal.set(Calendar.DAY_OF_MONTH, 1);
+		Date start = startCal.getTime();
+		//当月最后一天
+		Calendar endCal = Calendar.getInstance();
+		endCal.setTime(sd.parse(date));
+		int maxCurrentMonthDay = endCal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		endCal.set(Calendar.DAY_OF_MONTH, maxCurrentMonthDay);
+		Date end = endCal.getTime();
+
+		int num = (int) ((end.getTime() - start.getTime()) / (1000 * 3600 * 24));
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(start);
+		list.add(sd.format(cal.getTime()));
+		for(int i = 1; i <= num; i ++) {
+			cal.add(Calendar.DATE, 1);
+			Date time = cal.getTime();
+			list.add(sd.format(time));
+		}
+		return list;
+	}
+
+	/**
+	 * 根据某一天获取这一年的数据
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static List<String> getAllYear(String date) throws Exception {
+		List<String> list = new ArrayList<>();
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(sd.parse(date));
+		int year = cal.get(Calendar.YEAR);
+		String[] monArr = {"-01","-02","-03","-04","-05","-06","-07","-08","-09","-10","-11","-12"};
+		for(int i = 0; i < monArr.length; i ++) {
+			list.add(year + monArr[i]);
+		}
+		return list;
+	}
+
+	/**
+	 * 判断某一天是周几
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static int isToday(String date) throws Exception {
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(sd.parse(date));
+		int i = cal.get(Calendar.DAY_OF_WEEK);
+		if (i == 1) {
+			i = 7;
+		} else {
+			i -= 1;
+		}
+		return i;
+	}
+
+	/**
+	 * 判断某一天是几月份
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static int isMonth(String date) throws Exception {
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(sd.parse(date));
+		return cal.get(Calendar.MONTH) + 1;
+	}
+
+	/**
+	 * 获取当前星期、前一星期、后一星期的第一天和最后一天
+	 * @param sdf
+	 */
+	public static void getWeekDay(SimpleDateFormat sdf){
+		//获取Calendar实例，java封装的表现，private类Calendar的构造函数，通过静态方法创建对象
+		Calendar calendar=Calendar.getInstance();
+		//获取当前时间并格式化
+		System.out.println("当前时间: "+sdf.format(calendar.getTime()));
+		//本周一
+		Calendar calendar1=Calendar.getInstance();
+		calendar1.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		System.out.println("本周一: "+sdf.format(calendar1.getTime()));
+		//本周日
+		Calendar calendar2=Calendar.getInstance();
+		//外国的星期天和我们的不在一周(外国是星期天到星期六为一个星期)
+		calendar2.add(Calendar.DAY_OF_WEEK, 7);
+		calendar2.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+		System.out.println("本周日: "+sdf.format(calendar2.getTime()));
+		//下周一
+		Calendar calendar3=Calendar.getInstance();
+		calendar3.add(Calendar.DAY_OF_MONTH, 7);
+		calendar3.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		System.out.println("下周一: "+sdf.format(calendar3.getTime()));
+		//下周日
+		Calendar calendar4=Calendar.getInstance();
+		calendar4.add(Calendar.DAY_OF_MONTH, 14);
+		calendar4.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+		System.out.println("下周日: "+sdf.format(calendar4.getTime()));
+		//上周一
+		Calendar calendar5=Calendar.getInstance();
+		calendar5.add(Calendar.DAY_OF_YEAR, -7);
+		calendar5.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		System.out.println("上周一: "+sdf.format(calendar5.getTime()));
+		//上周日
+		Calendar calendar6=Calendar.getInstance();
+		calendar6.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+		System.out.println("上周日: "+sdf.format(calendar6.getTime()));
+	}
+
+	/**
+	 * 获取当前月、前一月、后一月的第一天和最后一天
+	 * @param sdf
+	 */
+	public static void getMonth(SimpleDateFormat sdf){
+		//定义当前月的总天数，比如30,31,28,29
+		int maxCurrentMonthDay=0;
+		Calendar calendar=Calendar.getInstance();
+		System.out.println("当前时间: "+sdf.format(calendar.getTime()));
+		//当月一号
+		Calendar calendar1=Calendar.getInstance();
+		calendar1.set(Calendar.DAY_OF_MONTH, 1);
+		System.out.println("本月第一天: "+sdf.format(calendar1.getTime()));
+		//当月最后一天
+		Calendar calendar2=Calendar.getInstance();
+		maxCurrentMonthDay=calendar2.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar2.set(Calendar.DAY_OF_MONTH, maxCurrentMonthDay);
+		System.out.println("本月最后一天: "+sdf.format(calendar2.getTime()));
+		//下个月一号
+		Calendar calendar3=Calendar.getInstance();
+		maxCurrentMonthDay=calendar3.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar3.add(Calendar.DAY_OF_MONTH, maxCurrentMonthDay);
+		calendar3.set(Calendar.DAY_OF_MONTH, 1);
+		System.out.println("下月第一天: "+sdf.format(calendar3.getTime()));
+		//下个月最后一天
+		Calendar calendar4=Calendar.getInstance();
+		maxCurrentMonthDay=calendar4.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar4.add(Calendar.DAY_OF_MONTH, maxCurrentMonthDay);
+		//第一个maxCurrentMonthDay获取的是当月的天数，第二个maxCurrentMonthDay获取的是下个月的天数
+		maxCurrentMonthDay=calendar4.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar4.set(Calendar.DAY_OF_MONTH, maxCurrentMonthDay);
+		System.out.println("下月第一天: "+sdf.format(calendar4.getTime()));
+		//上个月一号
+		Calendar calendar5=Calendar.getInstance();
+		maxCurrentMonthDay=calendar5.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar5.add(Calendar.DAY_OF_MONTH, -maxCurrentMonthDay);
+		calendar5.set(Calendar.DAY_OF_MONTH, 1);
+		System.out.println("上月第一天: "+sdf.format(calendar5.getTime()));
+		//上个月最后一天
+		Calendar calendar6=Calendar.getInstance();
+		maxCurrentMonthDay=calendar6.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar6.add(Calendar.DAY_OF_MONTH, -maxCurrentMonthDay);
+		//第一个maxCurrentMonthDay获取的是当月的天数，第二个maxCurrentMonthDay获取的是上个月的天数
+		maxCurrentMonthDay=calendar6.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar6.set(Calendar.DAY_OF_MONTH, maxCurrentMonthDay);
+		System.out.println("上月第一天: "+sdf.format(calendar6.getTime()));
+	}
+
+	/**
+	 * 根据某一天（周N），获取周一~周N的数据
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static Map<String, String> getWeek(String date) throws Exception {
+		Map<String, String> map = new LinkedHashMap<>();
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		int[] week = {Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, Calendar.THURSDAY, Calendar.FRIDAY, Calendar.SATURDAY, Calendar.SUNDAY};
+		String[] weekStr = {"周一","周二","周三","周四","周五","周六","周日"};
+		for (int i = 0; i < isToday(date); i ++) {
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(sd.parse(date));
+			cal.set(Calendar.DAY_OF_WEEK, week[i]);
+			if (week[i] == Calendar.SUNDAY) {
+				cal.add(Calendar.DAY_OF_WEEK, 7);
+			}
+			map.put(weekStr[i], sd.format(cal.getTime()));
+		}
+		return map;
+	}
+
+	/**
+	 * 根据某一天N，获取某月1号~N号的数据
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static List<String> getMonth(String date) throws Exception {
+		List<String> list = new ArrayList<>();
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		//当月一号
+		Calendar startCal = Calendar.getInstance();
+		startCal.setTime(sd.parse(date));
+		startCal.set(Calendar.DAY_OF_MONTH, 1);
+		Date start = startCal.getTime();
+		//最后一天
+		Date end = sd.parse(date);
+
+		int num = (int) ((end.getTime() - start.getTime()) / (1000 * 3600 * 24));
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(start);
+		list.add(sd.format(cal.getTime()));
+		for(int i = 1; i <= num; i ++) {
+			cal.add(Calendar.DATE, 1);
+			Date time = cal.getTime();
+			list.add(sd.format(time));
+		}
+		return list;
+	}
+
+	/**
+	 * 根据某一天N，获取某年1月~N月的数据
+	 *
+	 * @Author yuanzhonglin
+	 * @since 2020/7/18
+	 */
+	public static List<String> getYear(String date) throws Exception {
+		List<String> list = new ArrayList<>();
+		SimpleDateFormat sd = new SimpleDateFormat(STANDARD_DATE_FORMAT);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(sd.parse(date));
+		int year = cal.get(Calendar.YEAR);
+		String[] monArr = {"-01","-02","-03","-04","-05","-06","-07","-08","-09","-10","-11","-12"};
+		for(int i = 0; i < isMonth(date); i ++) {
+			list.add(year + monArr[i]);
+		}
+		return list;
+	}
 }
