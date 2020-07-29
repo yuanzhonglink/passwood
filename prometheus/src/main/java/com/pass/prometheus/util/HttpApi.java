116c3f5ad9a2b629d5dd2c011df49f2bc8a0bb6a
yuanzhonglin <yuanzhonglin@beyondcent.com>
2020-07-29 20:07:07 +0800

    websocket

---
+++ prometheus/src/main/java/com/pass/prometheus/util/HttpApi.java
@@ -0,0 +1,12 @@
+package com.pass.prometheus.util;
+
+/**
+ * @author yuanzhonglin
+ * @date 2020/7/29
+ * @Description:
+ */
+public class HttpApi {
+    public static void main(String[] args) {
+
+    }
+}
